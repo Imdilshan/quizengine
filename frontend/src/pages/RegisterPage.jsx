2adae83aba892d26c9189bec8eb326ef13d30fb4
Md Dilshan <imdilshan@proton.me>
2026-04-04 03:13:36 +0530

    Update RegisterPage.jsx

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -12,7 +12,7 @@ export default function RegisterPage() {
     console.log("FORM SUBMITTED");
 
     try {
-      const res = await fetch("http://localhost:5001/api/auth/register", {
+      const res = await fetch("https://quizengine-makv.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
